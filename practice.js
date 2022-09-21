2f2d7c0aa21d4f36439508094dffc40b10c1ca8d
“Mohamed-salman” <sqweezy.m@gmail.com>
2022-09-21 17:59:30 +0300

    done

--- practice.js
+++ practice.js
@@ -3,7 +3,14 @@ console.log("practice");
 // Write a function called 'average' that accepts a list of
 // numbers and returns the average. The average
 // of an empty list should be considered zero.
-function average() {
+function average(myArray) {
+  if (FileList.length === 0) {
+    return 0;
+  }
+  let sum = 0;
+  for (let i = 0; i < FileList.length; i++) {
+    sum += list[i];
+  }
   return undefined;
 }
 console.log(average([1, 2, 3]))
@@ -12,8 +19,17 @@ console.log(average([]))
 // Write a function called `biggest` that returns the
 // biggest number in an array. If there is no biggest
 // number the function should return undefined.
-function biggest() {
+function biggest(list) {
+if (list.length === 0) {
   return undefined;
+}
+let biggest = list[0];
+for (var i = 0, i < list.length; i++) {
+if (list[i] > biggest) {
+  biggest = list[1];
+}
+}
+return biggest;
 }
 console.log(biggest([12,3,4,567,8]))
 console.log(biggest([]))
@@ -39,6 +55,16 @@ console.log(biggest([]))
 // {q: 'cats', type: 'images', format: 'gif'}
 
 function urlParams(query) {
+  var url = query.split('?');
+  if (url.length === 1) {
+    return{};
+  }
+let params = url[1].split('&');
+let obj = {};
+for (var i=0; i < params.length; ++i) {
+  var keyVal = params[i].split('=');
+  obj[keyVal[0]] = keyVal[1];
+}
   return undefined;
 }
 console.log(urlParams("http://google.com/"))
