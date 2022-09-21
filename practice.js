60db5ad8bbe283962413f2a51017da6ee43e8d88
kawtharnaser <kawtharnasser7@gmail.com>
2022-09-21 20:20:43 +0300

    java practice completed

--- practice.js
+++ practice.js
@@ -3,20 +3,50 @@ console.log("practice");
 // Write a function called 'average' that accepts a list of
 // numbers and returns the average. The average
 // of an empty list should be considered zero.
-function average() {
-  return undefined;
+
+let sum=0
+function average(array){
+  if(array.length ==0){
+    return 0
+  }
+  else{
+    for(let i=0; i<array.length; i++){
+      sum = sum + array[i]
+    }
+    avg= sum/array.length
+    return avg
+    
+  }
 }
-console.log(average([1, 2, 3]))
+
+console.log(average([2, 9, 3]))
 console.log(average([]))
+
+
+
 
 // Write a function called `biggest` that returns the
 // biggest number in an array. If there is no biggest
 // number the function should return undefined.
-function biggest() {
-  return undefined;
+function biggest(array){
+  if(array.length ==0){
+    return undefined
+  }
+  else{
+    let biggestNum=array[0]
+    for(let i=1; i<array.length; i++){
+      if(array[i]> biggestNum){
+        biggestNum = array[i]
+      }
+    }
+    return biggestNum
+    
+  }
 }
+
 console.log(biggest([12,3,4,567,8]))
 console.log(biggest([]))
+
 
 // Write a function called `urlParams` that accepts a
 // string representing a URL and returns an object with
@@ -39,7 +69,17 @@ console.log(biggest([]))
 // {q: 'cats', type: 'images', format: 'gif'}
 
 function urlParams(query) {
-  return undefined;
+  let url = query.split('?')
+  if (url.length === 1) {
+    return {}
+  }
+  let params = url[1].split('&')
+  let obj = {}
+  for (let i = 0; i < params.length; i++) {
+    let keyVal = params[i].split('=')
+    obj[keyVal[0]] = keyVal[1]
+  }
+  return obj
 }
 console.log(urlParams("http://google.com/"))
 console.log(urlParams("http://google.com/?q=cats"))
