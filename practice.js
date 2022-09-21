152929de3becaad0100f7047fad90213ba02fd77
Ameera Ali <amiraabdullamir@gmail.com>
2022-09-22 01:55:23 +0300

    Functions answers added

--- practice.js
+++ practice.js
@@ -1,48 +1,41 @@
-console.log("practice");
 
 // Write a function called 'average' that accepts a list of
 // numbers and returns the average. The average
 // of an empty list should be considered zero.
-function average() {
-  return undefined;
+function average(arr) {
+  let sum =0 ;
+  sum =arr.reduce((num1,num2)=> num1+num2)
+  return sum/arr.length
 }
-console.log(average([1, 2, 3]))
-console.log(average([]))
+function biggest(arr) {
+let biggest = 0;
+for (let i=0;i<arr.length;i++){
+  if(arr[i]>biggest){
+    biggest = arr[i]
+    
+  }else if(biggest == arr[i] && i==arr.length-1){
+    return undefined;
 
-// Write a function called `biggest` that returns the
-// biggest number in an array. If there is no biggest
-// number the function should return undefined.
-function biggest() {
-  return undefined;
+  }
+  
 }
-console.log(biggest([12,3,4,567,8]))
-console.log(biggest([]))
-
-// Write a function called `urlParams` that accepts a
-// string representing a URL and returns an object with
-// key, value pairs for each parameter in the url string.
-// If there are no URL parameters your function should
-// return an empty object.
-//
-// Use the string methods split() to target parts of the
-// string to turn into an array
-// 
-// URLs are strings like this:
-//
-// http://google.com/
-// {}
-// http://google.com/?q=cats
-// {q: 'cats'}
-// http://google.com/?q=cats&type=images
-// {q: 'cats', type: 'images'}
-// http://google.com/?q=cats&type=images&format=gif
-// {q: 'cats', type: 'images', format: 'gif'}
-
-function urlParams(query) {
-  return undefined;
+return biggest;
 }
+
+let urlParams = (urlString) =>{
+  let urlObj = {}
+  if(!urlString.includes("=")){
+    return "doesn't contain any parameters"
+  }else{
+    let test = urlString.split("&")
+    test = test.map(element => element.split("="))
+    test [0][0]= test[0][0][test[0][0].length-1]
+    urlObj =  Object.fromEntries(test)
+return urlObj
+  }
+    
+
+}
+
 console.log(urlParams("http://google.com/"))
-console.log(urlParams("http://google.com/?q=cats"))
-console.log(urlParams("http://google.com/?q=cats&type=images"))
-console.log(urlParams("http://google.com/?q=cats&type=images&format=gif"))
 
