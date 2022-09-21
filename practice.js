33194a2059b96fc6090d2ecfdb25fe7e295ac3d7
Anwaar <anwaaralsyouf@gmail.com>
2022-09-21 22:06:29 +0300

    all problems are solved!

--- practice.js
+++ practice.js
@@ -25,16 +25,16 @@ console.log(average([]))
 // number the function should return undefined.
 function biggest(arr) {
   let max = 0
-  if(arr.length === 0){
+  if (arr.length === 0) {
     return undefined
   }
-  else{
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > max) {
-      max = arr[i]
-    } 
-  }
-  return max
+  else {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > max) {
+        max = arr[i]
+      }
+    }
+    return max
   }
 }
 console.log(biggest([12, 3, 4, 567, 8]))
@@ -61,10 +61,38 @@ console.log(biggest([]))
 // {q: 'cats', type: 'images', format: 'gif'}
 
 function urlParams(query) {
-  return undefined;
+
+  let newObj = {}
+  
+  //seperate the URL :
+  let arr1 = query.split('?')
+  //in case of no parameters in the URL:
+  if(arr1.length === 1){
+    return newObj
+  }
+    
+  else {
+    //array containing only the keys and values:
+    let arr2 = arr1[1].split('&')
+
+    //for loop to iterate between the pairs of key and value
+    for (let i = 0; i < arr2.length; i++) {
+
+      // seperate keys from values, now we have like an array inside an array [[0,1],[0,1],[0,1]] -> somthin like that
+      let parameters = arr2[i].split('=')  
+      let keyParam = parameters[0] //store zero index as a key
+      let valueParam = parameters[1] //store first index as value
+      
+      //creates an object with keys and values
+      newObj[keyParam] = valueParam  
+      
+    }
+
+  return newObj
 }
+}
+
 console.log(urlParams("http://google.com/"))
 console.log(urlParams("http://google.com/?q=cats"))
 console.log(urlParams("http://google.com/?q=cats&type=images"))
 console.log(urlParams("http://google.com/?q=cats&type=images&format=gif"))
-
