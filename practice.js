4613bcb3b9f1439e675fa4a473c27bd9f14b4e9d
fjalafoo <fjalafoo@gmail.com>
2022-09-21 15:37:15 +0300

    solved first few

--- practice.js
+++ practice.js
@@ -3,8 +3,24 @@ console.log("practice");
 // Write a function called 'average' that accepts a list of
 // numbers and returns the average. The average
 // of an empty list should be considered zero.
-function average() {
-  return undefined;
+function average(numbers) {
+  let sum = 0
+  let count = 0
+  let average = 0
+
+  if (numbers.length <= 0) {
+    return average = 0
+  }
+
+  else {
+    for (let i = 0; i < numbers.length; i++) {
+      sum += numbers[i]
+      count++
+    }
+    average = sum / count
+
+    return average;
+  }
 }
 console.log(average([1, 2, 3]))
 console.log(average([]))
@@ -12,10 +28,22 @@ console.log(average([]))
 // Write a function called `biggest` that returns the
 // biggest number in an array. If there is no biggest
 // number the function should return undefined.
-function biggest() {
-  return undefined;
+function biggest(arr) {
+  let big = 0
+  if (arr.length <= 0) {
+    return undefined
+  }
+  else {
+    for (let i = 0; i < arr.length; i++) {
+      if (big < arr[i]) {
+        big = arr[i]
+      }
+    }
+    return big
+  }
+
 }
-console.log(biggest([12,3,4,567,8]))
+console.log(biggest([12, 3, 4, 567, 8]))
 console.log(biggest([]))
 
 // Write a function called `urlParams` that accepts a
