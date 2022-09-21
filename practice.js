b39d6851ca7e1e59f9e3134eff22d142d9db0c2d
Nathujohns <113826663+Nathujohns@users.noreply.github.com>
2022-09-21 21:35:18 +0300

    Update practice.js

--- practice.js
+++ practice.js
@@ -3,20 +3,37 @@ console.log("practice");
 // Write a function called 'average' that accepts a list of
 // numbers and returns the average. The average
 // of an empty list should be considered zero.
-function average() {
-  return undefined;
+arry = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+function Average(array) {
+    var total = 0;
+    var count = 0;
+    array.forEach(function(item, index) {
+        total += item;
+        count++;
+    });
+    return total / count;
 }
-console.log(average([1, 2, 3]))
-console.log(average([]))
+console.log(Average(arry));
 
 // Write a function called `biggest` that returns the
 // biggest number in an array. If there is no biggest
 // number the function should return undefined.
-function biggest() {
-  return undefined;
+function biggest(arr) {
+  var longestNumber = -Infinity;
+ for (var i = 0; i < arr.length; i++) {
+   if (typeof arr[i] === 'number') {
+     if (arr[i] > longestNumber) {
+       longestNumber = arr[i];
+     }
+   }
+ }
+ if (longestNumber == -Infinity) {
+   longestNumber = 0;
+ }
+  return longestNumber;
 }
-console.log(biggest([12,3,4,567,8]))
-console.log(biggest([]))
+var output1 = biggest([3, 10, 5, 20, 3, 1]);
+console.log(output1);
 
 // Write a function called `urlParams` that accepts a
 // string representing a URL and returns an object with
@@ -39,7 +56,14 @@ console.log(biggest([]))
 // {q: 'cats', type: 'images', format: 'gif'}
 
 function urlParams(query) {
-  return undefined;
+  let urlString = 
+"https://github.com/Hussain-ai?tab=repositories";
+        let paramString = urlString.split('?')[1];
+        let queryString = new URLSearchParams(paramString);
+        for(let pair of queryString.entries()) {
+            console.log("Key is:" + pair[0]);
+            console.log("Value is:" + pair[1]);
+        }
 }
 console.log(urlParams("http://google.com/"))
 console.log(urlParams("http://google.com/?q=cats"))
