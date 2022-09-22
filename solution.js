f37ab9e25296342e37a71044a80c8a9a89e737d8
MramRashidR <mramrashid98@gmail.com>
2022-09-22 08:59:38 +0300

    num2

--- solution.js
+++ solution.js
@@ -10,3 +10,22 @@ let average = (arr) =>  {
     return avg 
   }
   
+
+
+
+
+  let biggestNum = (bigger) => {
+    let larg = bigger[i]
+    for (let i=0 ; i<bigger.length; i++) {
+      if ( larg > bigger[i]) {
+        larg = bigger[i] 
+      }
+    }
+  
+    return larg 
+  
+  }
+  
+  if (bigger.length === 0) {
+    return undefined
+  }
