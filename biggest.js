f14494bf06b8ff4e42effd65541ed5d8e4b980a5
Fuad AlShuroogi <fmjs@Fuads-MacBook-Air.local>
2022-09-22 11:37:56 +0300

    Fuad AlShuroogi - Uploaded the biggest.js

--- biggest.js
+++ biggest.js
@@ -1,1 +1,17 @@
-//Will work on it later .. 
+function biggest(num) {
+
+    if(Math.max(...num) === -Infinity){
+      //param supplied is empty hence returning undefined!
+      return undefined
+    } else if (num.length === 1){
+      //if only one number is supplied
+    return "Undefined - No biggest number to compare with !"
+    } else{
+        return Math.max(...num) 
+    
+    }
+    }
+      
+    console.log(biggest([12, 3, 4, 567, 8]))
+    console.log(biggest([7, 89, 90, 2, 6]))
+    console.log(biggest([100]))
