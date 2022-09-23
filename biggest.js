39d4fd4899226c0c92b5dc87a1fe72ddd04acd69
Fuad AlShuroogi <fmjs@Fuads-MacBook-Air.local>
2022-09-23 13:11:39 +0300

    Fuad AlShuroogi - Uploaded urlParams.js and modified biggest.js

--- biggest.js
+++ biggest.js
@@ -1,17 +1,17 @@
 function biggest(num) {
+  //Spread syntax ...
+  if(Math.max(...num) === -Infinity){
+    //param supplied is empty hence returning undefined!
+    return undefined
+  } else if (num.length === 1){
+    //if only one number is supplied
+  return "Undefined - No biggest number to compare with !"
+  } else{
+      return Math.max(...num) 
 
-    if(Math.max(...num) === -Infinity){
-      //param supplied is empty hence returning undefined!
-      return undefined
-    } else if (num.length === 1){
-      //if only one number is supplied
-    return "Undefined - No biggest number to compare with !"
-    } else{
-        return Math.max(...num) 
-    
-    }
-    }
-      
-    console.log(biggest([12, 3, 4, 567, 8]))
-    console.log(biggest([7, 89, 90, 2, 6]))
-    console.log(biggest([100]))
+  }
+  }
+
+  console.log(biggest([12, 3, 4, 567, 8]))
+  console.log(biggest([7, 89, 90, 2, 6,120]))
+  console.log(biggest([100]))
--- urlParams.js
+++ urlParams.js
@@ -1,1 +1,46 @@
-//Will work on it later .. 
+// Write a function called `urlParams` that accepts a
+// string representing a URL and returns an object with
+// key, value pairs for each parameter in the url string.
+// If there are no URL parameters your function should
+// return an empty object.
+
+// Use the string methods split() to target parts of the
+// string to turn into an array
+
+// URLs are strings like this:
+
+// http://google.com/
+// {}
+// http://google.com/?q=cats
+// {q: 'cats'}
+// http://google.com/?q=cats&type=images
+// {q: 'cats', type: 'images'}
+// http://google.com/?q=cats&type=images&format=gif
+// {q: 'cats', type: 'images', format: 'gif'}
+
+function urlParams(query) {
+    let url = query.split('?');
+    
+    let doesQueryParamExist = query.includes('?');
+    
+    let result = {};
+    
+    if (doesQueryParamExist){
+              let parameter = url[1].split('&');
+              for (let i = 0;i < parameter.length; i++){
+              let key = parameter[i].split('='); 
+              result[key[0]] = key[1]           
+    }
+              return result
+    } else {
+    //No query parameter is found hence returning an empty object ..
+              return {}
+    }
+    }
+
+//Test ..
+console.log(urlParams("https://github.com/?author=FuadAlShuroogi&repo-content=js-practice-problems"))
+console.log(urlParams("http://google.com/"))
+console.log(urlParams("http://google.com/?q=cats"))
+console.log(urlParams("http://google.com/?q=cats&type=images"))
+console.log(urlParams("http://google.com/?q=cats&type=images&format=gif"))
